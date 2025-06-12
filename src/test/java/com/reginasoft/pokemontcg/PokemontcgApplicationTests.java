ee12e4da901b6d5a33ac395540bd8822e6b1c7af
DyhegoS <dyhegodasilva@gmail.com>
2025-06-12 10:05:32 -0300

    Primeiro commit

---
+++ src/test/java/com/reginasoft/pokemontcg/PokemontcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reginasoft.pokemontcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemontcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
